d2a8b9c38a103c044d83d46c4fed3c4038836a67
Kathiresan M <kmkathirr10@gmail.com>
2025-12-22 18:07:47 +0530

    Add SR Flip-Flop module in Verilog

---
+++ SR_FF.v
@@ -0,0 +1,21 @@
+module srff(s,r,rst,clk,q,qb);
+  input s,r,rst, clk;
+  output reg q;
+ output qb;
+  always@(posedge clk)
+    begin
+      if(rst)
+        q<=1'bz;
+      else 
+        begin
+          case({s,r})
+            2'b00:q<=q;
+            2'b01:q<=1'b0;
+            2'b10:q<=1'b1;
+            2'b11:q<=1'bz;
+            default: q<=1'bx;
+          endcase
+        end
+    end
+    assign qb=~q;
+endmodule
