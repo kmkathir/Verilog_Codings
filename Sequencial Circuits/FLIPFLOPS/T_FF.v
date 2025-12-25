26b45bfa2db3c7f0bfcbeb847991deb1e1d54542
kmkathir <kmkathirr10@gmail.com>
2025-12-25 21:01:30 +0530

    design code for tff done

--- Sequencial Circuits/FLIPFLOPS/T_FF.v
+++ Sequencial Circuits/FLIPFLOPS/T_FF.v
@@ -3,3 +3,25 @@
 
 /************ Design Code *********************/
 
+module Tff (T,clk,rst,q,qb);
+  input T,clk,rst;
+  output reg q;
+  output qb;
+  
+  always@(posedge clk or posedge rst)
+    begin
+      if(rst)
+       q<=0;
+      else
+        begin
+        if(T==0)
+          q<=q;
+      else if (T==1)
+          q<=~q;
+      else
+        q<=1'bx;  //put 0 for toggle understanding
+        end
+    end
+  assign qb=~q; 
+endmodule
+  
