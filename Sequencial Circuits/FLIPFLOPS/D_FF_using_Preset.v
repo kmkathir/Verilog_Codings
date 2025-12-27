629a59510fc0c03990fd8d27978ac3341e88e40f
kmkathir <kmkathirr10@gmail.com>
2025-12-27 23:06:36 +0530

    tb coded

--- Sequencial Circuits/FLIPFLOPS/D_FF_using_Preset.v
+++ Sequencial Circuits/FLIPFLOPS/D_FF_using_Preset.v
@@ -26,3 +26,27 @@ module dff_pr_test;
   reg d,clk,rst,pr;
   wire q;
   wire qb;
+  dff_pr d1 (d,clk,rst,pr,q,qb);
+  always #5 clk = ~clk;
+  
+  initial
+   begin
+     clk = 0; rst = 1;
+     #10 rst = 0;
+     #10 pr=1;
+     #10 d = 0;
+     
+     #40 pr=0;
+     #30 d = 1;
+     #20 d = 0;
+     
+     #10 $finish;
+   end
+  initial
+    $monitor($time, "  rst=%b...pr=%b....Clk=%b....d=%b....q=%b....qb=%b", rst,pr, clk, d, q, qb);
+  initial
+    begin
+    $dumpfile("prg.vcd");
+      $dumpvars(0,dff_pr_test);
+    end
+endmodule 
