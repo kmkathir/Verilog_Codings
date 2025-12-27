5a3543cfb5c8593f1dcd324880896c756ce1d205
kmkathir <kmkathirr10@gmail.com>
2025-12-27 22:56:05 +0530

    design code for tusing jk added

--- Sequencial Circuits/FLIPFLOPS/T_using_JK.v
+++ Sequencial Circuits/FLIPFLOPS/T_using_JK.v
@@ -1,3 +1,17 @@
 //Designing a T Flip Flop using JK Flip Flop
-// EDA PLAYGROUND LINK : https://edaplayground.com/x/AgFB 
+// EDA PLAYGROUND LINK : https://edaplayground.com/x/AgFB
 
+/************ Design Code *********************/
+
+`include "jkff.sv"
+module Tff (T,clk,rst,q,qb);
+  input T,clk,rst;
+  output reg q;
+  output qb;
+  
+  jkff j1 (.j(T), .k(T), .rst(rst), .clk(clk), .q(q), .qb(qb));
+  
+endmodule
+  
+
+  
