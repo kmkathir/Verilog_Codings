6cf7a9797cc91f1aa6fe30d4faad5dbbe4384dbe
kmkathir <kmkathirr10@gmail.com>
2025-12-28 23:10:01 +0530

    new file created and design code done

---
+++ Sequencial Circuits/COUNTERS/BCD_updown_count.v
@@ -0,0 +1,30 @@
+//Design a 4-bit up-down BCD counter using behavioral modeling
+// EDA PLAYGROUND LINK : 
+
+/************ Design Code *********************/
+
+module cntr_4bit(clk,rst,q,qb);
+  input clk,rst;
+  output reg [3:0]q;
+  output [3:0] qb;
+  
+  always@(posedge clk or posedge rst)
+    begin
+      if(rst)
+       // q<=4'b0000; //up count 
+      q<=4'b1001; //down count
+      
+      else
+        begin
+      /*   q<=q+1; //up count
+          if(q==4'b1001)
+         q<=0;*/
+   
+                     q<=q-1;
+         			 if (q==4'b0000)
+                     q<=4'b1001; 
+        end
+    
+    end
+  assign qb=~q;
+endmodule
