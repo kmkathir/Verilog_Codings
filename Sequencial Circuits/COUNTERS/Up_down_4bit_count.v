cbff1b59eff55772b2e2a62cf3607326404a8ad5
kmkathir <kmkathirr10@gmail.com>
2025-12-28 22:59:08 +0530

    design code added for counter

--- Sequencial Circuits/COUNTERS/Up_down_4bit_count.v
+++ Sequencial Circuits/COUNTERS/Up_down_4bit_count.v
@@ -2,3 +2,21 @@
 // EDA PLAYGROUND LINK : https://edaplayground.com/x/Ryar 
 
 /************ Design Code *********************/
+
+module upcounter_4bit(clk,rst,q,qb);
+  input clk,rst;
+  output reg [3:0]q;
+  output [3:0] qb;
+  
+  always@( posedge clk or posedge rst )
+    begin 
+      if(rst)
+        q<=4'b0000;  //upcount
+      // q<=4"b1111; //downcount
+      else 
+        q<=q+1;
+      
+      //q<=q-1;        //downcount
+    end
+  assign qb=~q;
+endmodule
