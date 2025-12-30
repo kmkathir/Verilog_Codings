fa19a526c879e73fd11f35062f1a02a24574ba4a
kmkathir <kmkathirr10@gmail.com>
2025-12-30 23:07:27 +0530

    code added

--- Sequencial Circuits/COUNTERS/Asyn_4bit_upcount.v
+++ Sequencial Circuits/COUNTERS/Asyn_4bit_upcount.v
@@ -39,4 +39,22 @@ module Tff (T,clk,rst,q,qb);
   jkff j1 (.j(T), .k(T), .rst(rst), .clk(clk), .q(q), .qb(qb));
   
 endmodule
+
+////// CODE FOR 4 BIT ASYNCHRONOUS UP COUNTER USING T FLIP FLOPS ///////////
+
+`include "tff.sv"
+module cntr_4bit(clk,rst,q,qb);
+  input clk,rst;
+  output reg [3:0]q;
+  output [3:0] qb;
+  
+  Tff a0 (.T(1'b1), .clk(clk),  .rst(rst), .q(q[0]), .qb(qb[0]));
+  Tff a1 (.T(1'b1), .clk(q[0]), .rst(rst), .q(q[1]), .qb(qb[1]));
+  Tff a2 (.T(1'b1), .clk(q[1]), .rst(rst), .q(q[2]), .qb(qb[2]));
+  Tff a3 (.T(1'b1), .clk(q[2]), .rst(rst), .q(q[3]), .qb(qb[3]));
+  
+    
+endmodule
+
+
   
