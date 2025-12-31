d3fd15e7f6a61e3bdb099f418ba828103a6a2692
Kathiresan M <kmkathirr10@gmail.com>
2025-12-31 21:35:03 +0530

    Update Asyn_4bit_upcount.v

--- Sequencial Circuits/COUNTERS/Asyn_4bit_upcount.v
+++ Sequencial Circuits/COUNTERS/Asyn_4bit_upcount.v
@@ -48,10 +48,7 @@ module cntr_4bit(clk,rst,q,qb);
   output reg [3:0]q;
   output [3:0] qb;
   
-  Tff a0 (.T(1'b1), .clk(clk),  .rst(rst), .q(q[0]), .qb(qb[0]));
-  Tff a1 (.T(1'b1), .clk(q[0]), .rst(rst), .q(q[1]), .qb(qb[1]));
-  Tff a2 (.T(1'b1), .clk(q[1]), .rst(rst), .q(q[2]), .qb(qb[2]));
-  Tff a3 (.T(1'b1), .clk(q[2]), .rst(rst), .q(q[3]), .qb(qb[3]));
+  
   
     
 endmodule
