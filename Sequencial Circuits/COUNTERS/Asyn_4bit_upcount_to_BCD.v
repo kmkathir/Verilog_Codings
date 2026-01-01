ecbab2f731ba7e37a1de8ac2a74c4bfc05588214
kmkathir <kmkathirr10@gmail.com>
2026-01-01 23:13:08 +0530

    new file added

---
+++ Sequencial Circuits/COUNTERS/Asyn_4bit_upcount_to_BCD.v
@@ -0,0 +1,4 @@
+//Designing 4 bit Asynchronous Up counter using T Flip-Flops made from JK Flip-Flops to BCD counter
+// EDA PLAYGROUND LINK : https://edaplayground.com/x/ADAK 
+
+/************ Design Code *********************/
