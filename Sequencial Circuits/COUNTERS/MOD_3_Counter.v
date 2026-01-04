c19baad1497486b36b43cf35f815fa274c7f9f36
kmkathir <kmkathirr10@gmail.com>
2026-01-04 22:33:46 +0530

    desogn code some done

--- Sequencial Circuits/COUNTERS/MOD_3_Counter.v
+++ Sequencial Circuits/COUNTERS/MOD_3_Counter.v
@@ -1,2 +1,8 @@
 //Design of MOD 3 Counter using Behavioral Modeling in Verilog
-// EDA PLAYGROUND LINK : https://edaplayground.com/x/Y57c 
+// EDA PLAYGROUND LINK : https://edaplayground.com/x/Y57c
+
+module mod3_cntr(clk,rst,q,qb);
+  input clk,rst;
+  output reg [3:0]q;
+  output [3:0] qb;
+  
