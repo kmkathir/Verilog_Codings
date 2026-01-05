8256e375e05459340c37d06870249e7a85245bc4
kmkathir <kmkathirr10@gmail.com>
2026-01-05 22:38:28 +0530

    tb some done

--- Sequencial Circuits/REGISTERS/SISO.v
+++ Sequencial Circuits/REGISTERS/SISO.v
@@ -1,3 +1,21 @@
 //Designing SISO (Serial In Serial Out) Shift Register using D Flip-Flops
 // EDA PLAYGROUND LINK : https://edaplayground.com/x/kjBW
 
+/************ Design Code *********************/
+
+//Design code for D Flip-Flop
+
+module dff (d,clk,rst,q,qb);
+  input d,clk,rst;
+  output reg q;
+  output qb;
+  
+  always@(posedge clk or posedge rst)
+    begin
+      if(rst)
+       q<=0;
+      else
+        q<=d;
+    end
+  assign qb=~q;
+endmodule
