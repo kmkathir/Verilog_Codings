828a37e6d6a1b10e552be270a578823425f3711d
kmkathir <kmkathirr10@gmail.com>
2026-01-06 23:36:28 +0530

    d ff code done

---
+++ Sequencial Circuits/REGISTERS/SIPO.v
@@ -0,0 +1,21 @@
+//Designing a Serial In Parallel Out Shift Register (SIPO) using D Flip-Flops
+// EDA PLAYGROUND LINK : https://edaplayground.com/x/h4tg
+
+/************ Design Code *********************/
+
+//Design code for D Flip-Flop
+
+module dff (d,clk,rst,q,qb);
+  input d,clk,rst;
+  output reg q;
+  output qb;
+  
+  always@(posedge clk or posedge rst)
+    begin
+      if(rst)
+       q<=0;
+      else
+        q<=d;
+    end
+  assign qb=~q;
+endmodule
