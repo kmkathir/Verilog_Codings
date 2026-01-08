ed633d2febe3e87872f8bc370223c3dea97c3b2b
kmkathir <kmkathirr10@gmail.com>
2026-01-08 17:57:54 +0530

    d ff code done

--- Sequencial Circuits/REGISTERS/PIPO.v
+++ Sequencial Circuits/REGISTERS/PIPO.v
@@ -5,3 +5,17 @@
 
 //Design code for D Flip-Flop
 
+module dff (d,clk,rst,q,qb);
+  input d,clk,rst;
+  output reg q;
+  output qb;
+  
+  always@(posedge clk or posedge rst)
+    begin
+      if(rst)
+       q<=0;
+      else
+        q<=d;
+    end
+  assign qb=~q;
+endmodule
