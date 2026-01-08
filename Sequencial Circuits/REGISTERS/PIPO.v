c415ccd9e3e9539e65df973a471888ab5341dd10
kmkathir <kmkathirr10@gmail.com>
2026-01-08 17:49:22 +0530

    new file added

---
+++ Sequencial Circuits/REGISTERS/PIPO.v
@@ -0,0 +1,7 @@
+//Designing a Parallel In Parallel Out (PIPO) Register using D Flip-Flops
+// EDA PLAYGROUND LINK : https://edaplayground.com/x/RBzj
+
+/************ Design Code *********************/
+
+//Design code for D Flip-Flop
+
