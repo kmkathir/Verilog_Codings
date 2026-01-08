0120d9d48aaa1d955c6c6b940eb77c86d636cee4
kmkathir <kmkathirr10@gmail.com>
2026-01-08 17:58:51 +0530

    design code some done

--- Sequencial Circuits/REGISTERS/PIPO.v
+++ Sequencial Circuits/REGISTERS/PIPO.v
@@ -19,3 +19,19 @@ module dff (d,clk,rst,q,qb);
     end
   assign qb=~q;
 endmodule
+
+//Design code for 4-bit Parallel In Parallel Out (PIPO) Register
+
+module pipo(pin,pout,clk,rst,S_L);
+  input clk,rst,S_L;
+  input [3:0] pin;
+  output [3:0] pout;
+  wire [3:0] q;
+  wire w0,w1,w2,w3,w4,w5,w6,w7,w8,w9;
+  
+  dff d0 (.d(pin[3]), .clk(clk), .rst(rst), .q(q[0]));
+  dff d1 (.d(w3),     .clk(clk), .rst(rst), .q(q[1]));
+  dff d2 (.d(w6),     .clk(clk), .rst(rst), .q(q[2]));
+  dff d3 (.d(w9),     .clk(clk), .rst(rst), .q(q[3]));
+  
+  
