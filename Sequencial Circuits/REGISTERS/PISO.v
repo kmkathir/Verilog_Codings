d11ae9e81df7b3075b9ab0a451f668358a11ebf7
kmkathir <kmkathirr10@gmail.com>
2026-01-09 22:49:37 +0530

    d ff code done

--- Sequencial Circuits/REGISTERS/PISO.v
+++ Sequencial Circuits/REGISTERS/PISO.v
@@ -4,3 +4,18 @@
 //************ Design Code *********************/
 
 //Design code for D Flip-Flop
+
+module dff (d,clk,rst,q,qb);
+  input d,clk,rst;
+  output reg q;
+  output qb;
+  
+  always@(posedge clk or posedge rst)
+    begin
+      if(rst)
+       q<=0;
+      else
+        q<=d;
+    end
+  assign qb=~q;
+endmodule
