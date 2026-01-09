97cf9c7abdae4dff304e20fe0688d8985068fad9
kmkathir <kmkathirr10@gmail.com>
2026-01-09 22:47:58 +0530

    new file added

---
+++ Sequencial Circuits/REGISTERS/PISO.v
@@ -0,0 +1,6 @@
+//Designing a Parallel In Serial Out Shift Register (PISO) using Verilog
+// EDA PLAYGROUND LINK : https://edaplayground.com/x/Ye3p 
+
+//************ Design Code *********************/
+
+//Design code for D Flip-Flop
