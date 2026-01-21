7ebed67f614930e7a4b277cf489a565c1f8bf4a4
kmkathir <kmkathirr10@gmail.com>
2026-01-21 19:55:59 +0530

    Design Code some coded

--- Sequencial Circuits/COUNTERS/Johnson_Counter.v
+++ Sequencial Circuits/COUNTERS/Johnson_Counter.v
@@ -1,3 +1,13 @@
 //Designing a Johnson Counter using Verilog
 // EDA PLAYGROUND LINK : https://www.edaplayground.com/x/USuE 
 
+/************ Design Code *********************/
+
+module john_cntr(Sout,clk,rst,q);
+  input clk,rst;
+  output reg [3:0]q;
+  output Sout;
+ 
+   
+  
+endmodule 
