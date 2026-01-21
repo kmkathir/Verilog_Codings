f83a161eff836f216325bace23f681f263328fd1
kmkathir <kmkathirr10@gmail.com>
2026-01-21 19:56:40 +0530

    Design code fully done

--- Sequencial Circuits/COUNTERS/Johnson_Counter.v
+++ Sequencial Circuits/COUNTERS/Johnson_Counter.v
@@ -8,6 +8,13 @@ module john_cntr(Sout,clk,rst,q);
   output reg [3:0]q;
   output Sout;
  
-   
+   always @(posedge clk or posedge rst) begin
+    if (rst)
+      q <= 4'b0000;                 // reset state
+    else
+      q <= {q[2:0], ~q[3]};         // Johnson counter operation
+  end
+
+  assign Sout =q[3];
   
 endmodule 
